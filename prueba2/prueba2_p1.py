493d62c22bf182e6752fea50650e11b824ba939d
vicentehuenunalumnosulagoscl <vicenteantonio.huenun@alumnos.ulagos.cl>
2024-06-27 14:27:56 -0400

    prueba2_p1.py

--- prueba2/prueba2_p1.py
+++ prueba2/prueba2_p1.py
@@ -1,23 +1,26 @@
 notas = []
 while True:
-    entrada = input("Ingrese una nota o pulsa '0' para finalizar: ")
+    entrada = input("Ingrese una nota o pulse '0' para finalizar: ")
     if entrada == '0':
         break
     try:
         nota = float(entrada)
-        notas.append(nota)
+        if 0 <= nota <= 7: 
+            notas.append(nota)
+        else:
+            print("Ingrese una nota entre 1 y 7 o pulse '0' para finalizar.")
     except ValueError:
-        print("ingrese un número válido o pulsa '0' para finalizar ")
+        print("Ingrese un número valido o pulse '0' para finalizar.")
 
 if notas:
-    CantidadNotas = len(notas)
-    PromedioNotas = sum(notas) / CantidadNotas
-    NotaMinima = min(notas)
-    NotaMaxima = max(notas)
+    cantidadNotas = len(notas)
+    promedioNotas = sum(notas) / cantidadNotas
+    notaMinima = min(notas)
+    notaMaxima = max(notas)
 
-    print("Cantidad de notas:", CantidadNotas)
-    print("Promedio de notas:", round(PromedioNotas, 2))
-    print("Nota minima:", NotaMinima)
-    print("Nota máxima:", NotaMaxima)
+    print("Cantidad de notas:", cantidadNotas)
+    print("Promedio de notas:", round(promedioNotas, 2))
+    print("Nota mínima:", notaMinima)
+    print("Nota máxima:", notaMaxima)
 else:
-    print("No se ingresaron notas")
+    print("No se ingresaron notas.")

54196e5331b25fe26c1241c6a39a693e13f0442f
vicentehuenunalumnosulagoscl <vicenteantonio.huenun@alumnos.ulagos.cl>
2024-06-27 14:19:22 -0400

    Update prueba2_p3.py

--- prueba2/prueba2_p3.py
+++ prueba2/prueba2_p3.py
@@ -1,18 +1,17 @@
-paisesCapitales = {
-    "España": "Madrid",
-    "México": "Ciudad de México",
-    "Chile": "Santiago",
-    "Argentina": "Buenos Aires",
-    "Colombia": "Bogotá"
-}
+paisesCapitales = {}
 
+while True:
+    pais = input("Ingrese un país o pulse '0' para terminar: ")
+    if pais.lower() == '0':
+        break
+    capital = input("Ingrese la capital de {}: ".format(pais))
+    paisesCapitales[pais] = capital
 nombreTurista = input("Ingrese el nombre del turista: ")
-paisProcedencia = input("Ingrese el pais de procedencia del turista: ")
+paisProcedencia = input("Ingrese el país de procedencia del turista: ")
 
 if paisProcedencia in paisesCapitales:
     capital = paisesCapitales[paisProcedencia]
-    mensaje = "El turista con el nombre " + nombreTurista + " viene del país "
-    + paisProcedencia + " y su capital es " + capital + "."
+    mensaje = "El turista con el nombre " + nombreTurista + " viene del país " + paisProcedencia + " y su capital es " + capital + "."
     print(mensaje)
 else:
-    print("No se encontró información de capital para el pais", paisProcedencia)
+    print("No se encontró información de capital para el país", paisProcedencia)
